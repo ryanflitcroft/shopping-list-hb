acc27d93b45be8f7d5ba3c739e9f9576e2ebfba0
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-16 15:24:41 -0800

    State set for user on load, based on token in local storage. Components AuthPage anf ListPage called conditionally for user.

--- src/App.js
+++ src/App.js
@@ -14,8 +14,14 @@ import './App.css';
 
 export default function App() {
   // track the user in state
+  const [user, setUser] = useState(localStorage.getItem('supabase.auth.token'));
+  console.log(user);
 
   // add a useEffect to get the user and inject the user object into state on load
+  useEffect(() => {
+    const data = getUser();
+    setUser(data);
+  }, [user]);
 
   async function handleLogout() {
     // call the logout function
@@ -27,11 +33,20 @@ export default function App() {
       <div className='App'>
         <header>
           {/* if there's a user, render a logout button here */}
+          {
+            user
+              && <button onClick={logout}>Logout</button>
+          }
         </header>
         <main>
           <Switch>
             <Route exact path="/">
               {/* if there is a user, redirect to the list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
+              {
+                user
+                  ? <ListPage />
+                  : <AuthPage setUser={setUser} />
+              }
             </Route>
             <Route exact path="/shopping-list">
               {/* if there's a user, take them to the list page. Otherwise, redirect them to the home/auth page */}
