30456c4f6e86502239b4c7638d27f308c0cf9666
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-16 16:06:10 -0800

    Router setup in component App for routes to AuthPage and ListPage.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import './App.css';
 export default function App() {
   // track the user in state
   const [user, setUser] = useState(localStorage.getItem('supabase.auth.token'));
-  console.log(user);
 
   // add a useEffect to get the user and inject the user object into state on load
   useEffect(() => {
@@ -25,6 +24,8 @@ export default function App() {
 
   async function handleLogout() {
     // call the logout function
+    await logout();
+    setUser('');
     // clear the user in state
   }
 
@@ -35,7 +36,7 @@ export default function App() {
           {/* if there's a user, render a logout button here */}
           {
             user
-              && <button onClick={logout}>Logout</button>
+              && <button onClick={handleLogout}>Logout</button>
           }
         </header>
         <main>
@@ -44,12 +45,17 @@ export default function App() {
               {/* if there is a user, redirect to the list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
               {
                 user
-                  ? <ListPage />
+                  ? <Redirect to='/shopping-list' />
                   : <AuthPage setUser={setUser} />
               }
             </Route>
             <Route exact path="/shopping-list">
               {/* if there's a user, take them to the list page. Otherwise, redirect them to the home/auth page */}
+              {
+                user
+                  ? <ListPage />
+                  : <Redirect to='/' />
+              }
 
             </Route>
           </Switch>
