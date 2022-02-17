b0a92fe1fd7805f518dbcb0fdc8a7ad2d9e27622
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-16 16:21:27 -0800

    rfc written for ListItemForm.

--- src/ListItemForm.js
+++ src/ListItemForm.js
@@ -3,39 +3,49 @@ import { createListItem } from './services/fetch-utils';
 
 export default function ListItemForm({ fetchItems }) {
   // you'll need to track the name and quantity in state
+  const [listItem, setListItem] = useState('');
+  const [quantity, setQuantity] = useState(1);
 
   async function handleSubmit(e) {
     e.preventDefault();
-    
+    const item = {
+      name: listItem,
+      quantity,
+      has_been_bought: false
+    };
     // make a new list item in supabase using the form values stored in state
-
-    // refetch the items using the handler functionpassed down as a prop
-
+    createListItem(item);
+    // refetch the items using the handler function passed down as a prop
+    fetchItems();
+    setListItem('');
+    setQuantity(1);
     // clear the name and quantity in state to refresh the form
+
   }
 
   return (
     <div className='new-item-form-container'>
       {/* on submit, call the handleSubmit function */}
-      <form>
+      <form onSubmit={handleSubmit}>
           I need . . . 
         <label>
             Quantity
           {/* on change, update the quantity in state */}
-          <input 
-            // this should be a controlled input, soi set the value based on state
-            required 
+          <input onChange={(e) => setQuantity(e.target.value)}
+            // this should be a controlled input, so set the value based on state
+            value={quantity}
             type="number" 
             name="quantity"
-          />
+            required />
         </label>
         <label>
             Name
           {/* on change, update the name in state */}
-          <input
-            // this should be a controlled input, soi set the value based on state 
-            required 
-            name="name" />
+          <input onChange={(e) => setListItem(e.target.value)}
+            // this should be a controlled input, so set the value based on state 
+            value={listItem}
+            name="name"
+            required />
         </label>
         <button>Add item</button>
       </form>
--- src/ListPage.js
+++ src/ListPage.js
@@ -7,14 +7,20 @@ export default function ListPage() {
   const [listItems, setShoppingList] = useState([]);
 
   // on load, call the fetchItems function (remember: useEffect)
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
   async function fetchItems() {
     // fetch the list items and inject them into state
+    const data = await getListItems();
+    setShoppingList(data);
   }
 
   async function handleDeleteClick() {
     // delete all items
-
+    await deleteAllItems();
+    fetchItems();
     // then call your fetchItems function to fetch and re-display
   }
 
@@ -22,6 +28,7 @@ export default function ListPage() {
     <div className="list-page">
       <button onClick={handleDeleteClick}>New List</button>
       {/* pass fetchItems to the ListItemForm component */}
+      <ListItemForm fetchItems={fetchItems} />
       <div className='item-list'>
         {/* map through all the list items and render them here */}
       </div>
